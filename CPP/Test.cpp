df6467921501e8d09a4d1ea1a08c51616d08c196
OliverRise12 <72047068+OliverRise12@users.noreply.github.com>
2023-10-04 11:58:39 +0200

    MAD?
    MAD?

--- CPP/Test.cpp
+++ CPP/Test.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <stdlib.h>   
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "Hello Docker container!" << std::endl;
+  int iSecret;
+
+  iSecret = rand() % 10 + 1;
+
+  for (int i = 0; i<10; i++){
+  iSecret = rand() % 10 + 1;
+  std::cout << iSecret << std::endl;
+  }
+
+
   return 0;
 }
